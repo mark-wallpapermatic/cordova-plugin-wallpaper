f9312a7cba476d6140e0fc34fccefc4ea28918d3
markibanez <mark.ibanez@gmail.com>
2019-12-13 09:12:28 +0800

    Wallpaper scaleCenterCrop

--- src/android/wallpaper.java
+++ src/android/wallpaper.java
@@ -70,7 +70,8 @@ public class wallpaper extends CordovaPlugin
 				bitmap = BitmapFactory.decodeStream(instr);
 			}
 
-			Bitmap adjusted = returnBitmap(bitmap, phoneWidth, phoneHeight);
+			//Bitmap adjusted = returnBitmap(bitmap, phoneWidth, phoneHeight);
+			Bitmap adjusted = scaleCenterCrop(bitmap, phoneWidth, phoneHeight);
 
 			WallpaperManager myWallpaperManager = WallpaperManager.getInstance(context);
 			myWallpaperManager.setBitmap(adjusted);
@@ -108,6 +109,32 @@ public class wallpaper extends CordovaPlugin
 		return background;
 	}
 
+	private Bitmap scaleCenterCrop(Bitmap source, int newHeight, int newWidth)
+	{
+        int sourceWidth = source.getWidth();
+        int sourceHeight = source.getHeight();
+
+        float xScale = (float) newWidth / sourceWidth;
+        float yScale = (float) newHeight / sourceHeight;
+        float scale = Math.max(xScale, yScale);
+
+        // Now get the size of the source bitmap when scaled
+        float scaledWidth = scale * sourceWidth;
+        float scaledHeight = scale * sourceHeight;
+
+        float left = (newWidth - scaledWidth) / 2;
+        float top = (newHeight - scaledHeight) / 2;
+
+        RectF targetRect = new RectF(left, top, left + scaledWidth, top
+                + scaledHeight);//from ww w  .j a va 2s. co m
+
+        Bitmap dest = Bitmap.createBitmap(newWidth, newHeight,
+                source.getConfig());
+        Canvas canvas = new Canvas(dest);
+        canvas.drawBitmap(source, null, targetRect, null);
+
+        return dest;
+    }
 	// private Bitmap returnBitmap(Bitmap originalImage, int width, int height)
 	// {
 	//     //Bitmap background = Bitmap.createBitmap((int)width, (int)height, Bitmap.Config.ARGB_8888);
